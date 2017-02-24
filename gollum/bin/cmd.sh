a86e48f4926bd3a15c08c0f98a32ff847ff71261
yuankunzhu <zhuyuank@gmail.com>
2017-02-24 12:28:11 -0500

    rm-gollum-bin

--- gollum/bin/cmd.sh
+++
@@ -1,8 +0,0 @@
-git clone git@github.com:your-account/your-project.wiki.git
-brew install icu4c
-sudo gem install gollum # willl hang on "Installing ri documentation for gollum-4.0.1" for a while
-git init & gollum
-
-# covert md to html
-brew install pandoc
-pandoc -f markdown some-file.md > some-file.html
--- gollum/bin/loop-md-to-html.sh
+++
@@ -1,6 +0,0 @@
-FILES="*.md"
-for f in $FILES
-do
-    base=`basename $f ".md"`
-    pandoc -s -f markdown $f > "$base".html
-done
