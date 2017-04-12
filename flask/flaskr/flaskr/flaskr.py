33f545714ffb4764d76b5e5153816006e98bc53b
yuankunzhu <zhuyuank@gmail.com>
2017-04-12 12:30:57 -0400

    add-flaskr

--- flask/flaskr/flaskr/flaskr.py
+++ flask/flaskr/flaskr/flaskr.py
@@ -14,17 +14,16 @@ local_cfg = dict(
 
 app = Flask(__name__)  # create the application instance :)
 app.config.from_object(__name__)  # load config from this file , flaskr.py
+
+
 # Load default config and override config from an environment variable
-
-
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'flaskr.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
-# app.config.from_pyfile
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)  # app.config.from_pyfile
 
 
 def connect_db():
@@ -32,3 +31,30 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
+if __name__ == '__main__':
+    app.run()
