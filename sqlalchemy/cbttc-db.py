e03d3322af6c686faf974081a248822c8d3563ca
yuankunzhu <zhuyuank@gmail.com>
2017-06-06 13:32:44 -0400

    :construction:cbttc db

--- sqlalchemy/cbttc-db.py
+++ sqlalchemy/cbttc-db.py
@@ -1,6 +1,7 @@
 import logging
 import argparse
 import ConfigParser
+import sqlalchemy
 
 
 def main():
@@ -8,8 +9,18 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('configfile')
     args = parser.parse_args()
-    configs = ConfigParser.ConfigParser(args.configfile)
-    configs.read(configs)
+    configs = ConfigParser.ConfigParser()
+    configs.read(args.configfile)
+    postgres = 'postgresql://{}:{}@{}/{}'.format(
+        configs.get('harvestdb', 'username'),
+        configs.get('harvestdb', 'password'),
+        configs.get('harvestdb', 'hostname'),
+        configs.get('harvestdb', 'database'))
+    engine = sqlalchemy.create_engine(postgres)
+    print engine
+    engine.connect()
+    # print sqlalchemy.MetaData()
+    engine.dispose()
 
 if __name__ == '__main__':
     main()
