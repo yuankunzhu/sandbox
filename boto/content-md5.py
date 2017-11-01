5d9e537be9152b439918a85ad81293696a02475d
Yuankun Zhu <zhuyuank@gmail.com>
2017-11-01 18:25:54 -0400

    add boto

--- boto/content-md5.py
+++ boto/content-md5.py
@@ -1,18 +1,18 @@
 import boto3
-session = boto3.Session(profile_name='saml')
-s3 = session.resource('s3')
+# session = boto3.Session(profile_name='saml')
+# s3 = session.resource('s3')
 
-object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/HKNY3CCXX-2.hgv.bam')
-print object.e_tag
+# object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/HKNY3CCXX-2.hgv.bam')
+# print object.e_tag
 
-object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/HLKMYCCXX-3.hgv.bam')
-print object.e_tag
+# object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/HLKMYCCXX-3.hgv.bam')
+# print object.e_tag
 
-object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/Manifest.txt')
-print object.e_tag
+# object = s3.Object('kids-first-seq-data','x01/bcm/batch_01/Manifest.txt')
+# print object.e_tag
 
 session = boto3.Session(profile_name='default')
 s3 = session.resource('s3')
 
-object = s3.Object('test.cavatica_2','chr37_list')
+object = s3.Object('cbttc.seq.data','NantWorks/008f259f-c6a4-49a1-b404-efe16afb3a8a.bam')
 print object.e_tag
